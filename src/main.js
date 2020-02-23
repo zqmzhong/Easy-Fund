aaf6aaafe447d081640a1f3ec083a0a07d6c6e4c
Francis Bell <zqmzhong@gmail.com>
2020-02-23 14:42:10 +0800

    Add i18n internationalization

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from "vue";
-import App from "./App.vue";
+import VueI18n from "vue-i18n";
 import vuetify from "./plugins/vuetify";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import App from "./App.vue";
+import en from "./assets/langs/en";
+import zh from "./assets/langs/zh";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+    locale: "zh",
+    messages: { zh, en },
+});
+
 new Vue({
     vuetify,
-    render: h => h(App)
+    i18n,
+    render: (h) => h(App),
 }).$mount("#app");
