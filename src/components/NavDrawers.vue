b047c7fdac1a1c2ab589e6bd22bea30345338711
Francis Bell <zqmzhong@gmail.com>
2020-02-23 14:43:09 +0800

    Add tabs and navigation drawers

---
+++ src/components/NavDrawers.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+    name: "NavDrawers",
+    data: () => ({
+        categories: [
+            ["optional", "steady", "theme", "research", "compare", "income", "manager", "fundBar"],
+            ["price", "ranking", "rating", "company", "estimation", "investRanking", "dividend", "private"],
+            ["currentPo", "indexPo", "buy", "steady", "ranking", "regularPo", "privatePo", "incomeRanking", "vip"],
+            ["currentPo", "regularPo", "indexPo", "privatePo", "incomeList", "steady", "vip", "insurance"],
+            ["help", "education", "advice", "safe"],
+        ],
+        categoryNames: ["hot", "data", "trade", "buy", "guide"],
+        icons: [],
+    }),
+};
+</script>
+
+<template>
+    <v-list>
+        <v-list-item>
+            <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <v-list-group
+            v-for="(category, index) in categories"
+            :key="index"
+            prepend-icon="account_circle"
+            value="true"
+        >
+            <template v-slot:activator>
+                <v-list-item-title class="font-face">
+                    {{ $t(`labels.${categoryNames[index]}`) }}
+                </v-list-item-title>
+            </template>
+
+            <v-list-item
+                v-for="(item, i) in category"
+                :key="i"
+                link
+            >
+                <v-list-item-title class="font-face" v-text="$t(`labels.${item}`)" />
+<!--                <v-list-item-icon>-->
+<!--                    <v-icon v-text="item" />-->
+<!--                </v-list-item-icon>-->
+            </v-list-item>
+        </v-list-group>
+    </v-list>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tab.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: "NavTab",
+    props: {
+        icon: { type: String, require: true },
+        text: { type: String, require: true },
+    },
+    computed: {
+        iconName() {
+            return `mdi-${this.icon}`;
+        },
+        displayText() {
+            return this.$t(`labels.${this.text}`);
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="card-tab ma-2">
+        <v-card hover>
+            <v-icon
+                color="red lighten-1"
+                size="60"
+            >
+                {{ iconName }}
+            </v-icon>
+        </v-card>
+        <v-sheet class="font-face text-center pa-0">
+            {{ displayText }}
+        </v-sheet>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .card-tab {
+        float: left;
+        align-items: center;
+    }
+</style>
