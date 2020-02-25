b373ffd67680f53e97bba52928b969a0c65766c4
Francis Bell <zqmzhong@gmail.com>
2020-02-25 23:19:09 +0800

    Adjust style

--- src/components/NavDrawers.vue
+++ src/components/NavDrawers.vue
@@ -2,6 +2,7 @@
 export default {
     name: "NavDrawers",
     data: () => ({
+        expand: true,
         categories: [
             ["optional", "steady", "theme", "research", "compare", "income", "manager", "fundBar"],
             ["price", "ranking", "rating", "company", "estimation", "investRanking", "dividend", "private"],
@@ -10,7 +11,7 @@ export default {
             ["help", "education", "advice", "safe"],
         ],
         categoryNames: ["hot", "data", "trade", "buy", "guide"],
-        icons: [],
+        icons: ["fire", "database", "currency-cny", "cart-outline", "account-group"],
     }),
 };
 </script>
@@ -28,8 +29,8 @@ export default {
         <v-list-group
             v-for="(category, index) in categories"
             :key="index"
-            prepend-icon="account_circle"
-            value="true"
+            :prepend-icon="`mdi-${icons[index]}`"
+            :value="index === 0"
         >
             <template v-slot:activator>
                 <v-list-item-title class="font-face">
@@ -40,17 +41,21 @@ export default {
             <v-list-item
                 v-for="(item, i) in category"
                 :key="i"
+                class="category-list"
                 link
             >
-                <v-list-item-title class="font-face" v-text="$t(`labels.${item}`)" />
-<!--                <v-list-item-icon>-->
-<!--                    <v-icon v-text="item" />-->
-<!--                </v-list-item-icon>-->
+                <v-list-item-title
+                    class="font-face subtitle-2"
+                    v-text="$t(`labels.${item}`)"
+                />
             </v-list-item>
         </v-list-group>
     </v-list>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.category-list {
+    padding-left: 72px;
+    min-height: 38px;
+}
 </style>
